00be940cebb2f3dd22bb033da7ad94778450474d
elizabethnnguyen <42651472+elizabethnnguyen@users.noreply.github.com>
2021-04-21 12:58:45 -0700

    Initial commit

---
+++ src/index2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import Container from "./loader";
+
+const App = () => <Container />;
+
+
+
+
+function App() {
+    console.log("hello is this working?");
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root2"));
---
+++ src/loader.js
@@ -0,0 +1,9 @@
+// Original: https://tympanus.net/codrops/2017/03/15/stack-motion-hover-effects/
+import React from 'react';
+import ReactDOM from "react-dom";
+import "./styles2.css";
+import { CoffeeLoading } from 'react-loadingg';
+
+const Container = () => <CoffeeLoading color="#000000" />;
+
+export default Container;
